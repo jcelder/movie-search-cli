19772cd2f1b592d11b40b3306a054961dc2fbc4a
Joshua Elder <aresx130@gmail.com>
2017-10-02 11:30:04 -0700

    Added test for urlSafe function.

--- test/movie-search_test.js
+++ test/movie-search_test.js
@@ -4,14 +4,17 @@ const exec = require('child_process').exec
 describe('movie-search.js', function() {
 
   describe('CLI', function() {
-
     it('should show USAGE when no argument is supplied', (done) => {
       exec('node movie-search.js', (error, stdout, stderr) => {
               expect(stderr).to.deep.include('USAGE')
               done()
       })
     })
-
   })
-
+  describe('urlSafe()', function() {
+    it('should return \'finding%20nemo\' when passed \'finding nemo\'', () => {
+      const urlSafe = require('../movie-search.js').urlSafe
+      expect(urlSafe('finding nemo')).to.deep.equal('finding%20nemo')
+    })
+  })
 })
