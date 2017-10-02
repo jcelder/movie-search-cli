5bbdb26bcad6e848d66e28ba78a11dcd4e2468ba
Joshua Elder <aresx130@gmail.com>
2017-10-02 11:30:26 -0700

    Added function stub for urlSafe().

--- movie-search.js
+++ movie-search.js
@@ -3,3 +3,10 @@ const cheerio = require('cheerio')
 
 const searchTerm = process.argv.slice(2).join(' ')
 if(!process.argv[2]) console.error('USAGE ERROR: Invalid or No Argument Supplied')
+
+const urlSafe = (str) => {
+  return str
+}
+
+
+module.exports = { urlSafe }
