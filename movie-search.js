21c69b284b9d0d2aca651e8b12240a8d1569eec1
Joshua Elder <aresx130@gmail.com>
2017-10-03 16:47:44 -0700

    Refactored code.

--- movie-search.js
+++ movie-search.js
@@ -2,16 +2,18 @@ const http = require('http')
 const cheerio = require('cheerio')
 
 const searchTerm = process.argv.slice(2).join(' ')
-if (!process.argv[2]) console.error('USAGE ERROR: Invalid or No Argument Supplied')
+if (!process.argv[2]) {
+  console.error('USAGE ERROR: Invalid or No Argument Supplied')
+  process.exit(1)
+}
 
 const urlSafe = (str) => {
   return str.replace(/ /g, '%2B')
 }
 
-const url = `http://www.imdb.com/find?ref_=nv_sr_fn&q=${urlSafe(searchTerm)}&s=all`
-const results = []
+const url = `http://www.im.com/find?ref_=nv_sr_fn&q=${urlSafe(searchTerm)}&s=all`
 
-const printSearchResults = () => {
+const printSearchResults = (results) => {
   results.forEach((str) => {
     console.log(str.slice(0, str.indexOf('-')))
   })
@@ -21,25 +23,25 @@ http.get(url, (res) => {
   const { statusCode } = res;
   let error
   if (statusCode !== 200) {
-    error = new Error(`Request Failed. \n
+    error = new Error(`Request Failed.
       Status Code: ${statusCode}`)
   }
   if (error) {
-    console.error(error)
+    console.error(error.message)
+    // consume response data to free up memory
     res.resume();
+    return
   }
 
   res.setEncoding('utf8')
   let rawData = ''
-  res.on('data', (chunk) => rawData += chunk )
+  res.on('data', chunk => rawData += chunk )
   res.on('end', () => {
     try {
       const $ = cheerio.load(rawData)
-      $('.findSection').first().find('.result_text').each(function (i, elem) {
-        results[i] = $(elem).text()
-      })
+      const results = $('.findSection').first().find('.result_text').map((i, elem) => $(elem).text())
       // console.log(results)
-      printSearchResults()
+      printSearchResults(results.toArray())
     } catch (e) {
       console.error(e.message)
     }
