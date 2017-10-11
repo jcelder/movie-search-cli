2670a90bf2be5bc89c9cb26fc65eb4ca406a10fd
Joshua Elder <aresx130@gmail.com>
2017-10-11 08:58:32 -0700

    Replaced node http module function call with request-promise module function calls.

--- movie-search.js
+++ movie-search.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const rp = require('request-promise')
 const cheerio = require('cheerio')
 
 const searchTerm = process.argv.slice(2).join(' ')
@@ -11,7 +11,14 @@ const urlSafe = (str) => {
   return str.replace(/ /g, '%2B')
 }
 
-const url = `http://www.im.com/find?ref_=nv_sr_fn&q=${urlSafe(searchTerm)}&s=all`
+const url = `http://www.imdb.com/find?ref_=nv_sr_fn&q=${urlSafe(searchTerm)}&s=all`
+
+const options = {
+  uri: url,
+  transform: (body) => {
+    return cheerio.load(body)
+  }
+}
 
 const printSearchResults = (results) => {
   results.forEach((str) => {
@@ -19,35 +26,42 @@ const printSearchResults = (results) => {
   })
 }
 
-http.get(url, (res) => {
-  const { statusCode } = res;
-  let error
-  if (statusCode !== 200) {
-    error = new Error(`Request Failed.
-      Status Code: ${statusCode}`)
-  }
-  if (error) {
-    console.error(error.message)
-    // consume response data to free up memory
-    res.resume();
-    return
-  }
+// http.get(url, (res) => {
+//   const { statusCode } = res;
+//   let error
+//   if (statusCode !== 200) {
+//     error = new Error(`Request Failed.
+//       Status Code: ${statusCode}`)
+//   }
+//   if (error) {
+//     console.error(error.message)
+//     // consume response data to free up memory
+//     res.resume();
+//     return
+//   }
+//
+//   res.setEncoding('utf8')
+//   let rawData = ''
+//   res.on('data', chunk => rawData += chunk )
+//   res.on('end', () => {
+//     try {
+//       const $ = cheerio.load(rawData)
+//       const results = $('.findSection').first().find('.result_text').map((i, elem) => $(elem).text())
+//       // console.log(results)
+//       printSearchResults(results.toArray())
+//     } catch (e) {
+//       console.error(e.message)
+//     }
+//   })
+// })
 
-  res.setEncoding('utf8')
-  let rawData = ''
-  res.on('data', chunk => rawData += chunk )
-  res.on('end', () => {
-    try {
-      const $ = cheerio.load(rawData)
-      const results = $('.findSection').first().find('.result_text').map((i, elem) => $(elem).text())
-      // console.log(results)
-      printSearchResults(results.toArray())
-    } catch (e) {
-      console.error(e.message)
-    }
+rp(options)
+  .then(($) => {
+    const results = $('.findSection').first().find('.result_text').map((i, elem) => $(elem).text())
+    printSearchResults(results.toArray())
   })
-})
-
-
+  .catch((err) => {
+    console.error(err.message)
+  })
 
 module.exports = { urlSafe }
